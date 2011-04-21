3976da099491d865938b9e7d55d85cdfd99fe810
CJ Kucera <pez@apocalyptech.com>
2011-04-21 00:14:45 -0500

    Let's also pause the batch file in case that one's clicked from Explorer

--- support/minecraft_xray.bat
+++ support/minecraft_xray.bat
@@ -1,1 +1,3 @@
 java -Xms256m -Xmx1024m -cp AppleJavaExtensions.jar;jinput.jar;lwjgl.jar;lwjgl_test.jar;lwjgl_util.jar;lwjgl_util_applet.jar;lzma.jar;xray.jar -Djava.library.path=. com.apocalyptech.minecraft.xray.XRay
+@echo.
+@pause
