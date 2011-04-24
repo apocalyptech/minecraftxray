03955ea8b58e5a0264bf402eab90b90a2ca9f646
CJ Kucera <pez@apocalyptech.com>
2011-04-24 15:45:26 -0500

    And let's go ahead and cut 3.0.1

--- src/com/apocalyptech/minecraft/xray/XRay.java
+++ src/com/apocalyptech/minecraft/xray/XRay.java
@@ -98,7 +98,7 @@ public class XRay
 	// are we inverting the mouse
 	private boolean invertMouse = false;
 	// window title
-	private final String app_version = "3.0.0";
+	private final String app_version = "3.0.1";
 	private final String app_name    = "Minecraft X-Ray";
 	private final String windowTitle = app_name + " " + app_version;
 
