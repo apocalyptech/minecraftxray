e2ddf9ced140fed99a4dc4edba054258c0870b91
CJ Kucera <pez@apocalyptech.com>
2011-09-12 16:21:32 -0500

    Package our main jar so that it's more directly runnable, and our lib files in their own directory

--- support/minecraft_xray.bat
+++ support/minecraft_xray.bat
@@ -1,6 +1,6 @@
 @echo. >> minecraft_xray_output_log.txt
-@echo Launching Minecraft X-Ray >> minecraft_xray_output_log.txt
-java -Xms256m -Xmx1024m -cp AppleJavaExtensions.jar;jinput.jar;lwjgl.jar;lwjgl_test.jar;lwjgl_util.jar;lwjgl_util_applet.jar;lzma.jar;xray.jar;snakeyaml-1.9.jar -Djava.library.path=. com.apocalyptech.minecraft.xray.XRay 1>> minecraft_xray_output_log.txt 2>&1
+@echo Launching Minecraft... X-Ray >> minecraft_xray_output_log.txt
+java -Xms256m -Xmx1024m -Djava.library.path=lib/native -jar xray.jar 1>> minecraft_xray_output_log.txt 2>&1
 @echo.
 @echo X-Ray log saved to minecraft_xray_output_log.txt
 @echo.
--- support/minecraft_xray.sh
+++ support/minecraft_xray.sh
@@ -3,7 +3,7 @@ LOGFILE=minecraft_xray_output_log.txt
 cd "`dirname "$0"`"
 echo >> ${LOGFILE}
 echo "Launching Minecraft X-Ray..." >> ${LOGFILE}
-java -Xms256m -Xmx1024m -cp AppleJavaExtensions.jar:jinput.jar:lwjgl.jar:lwjgl_test.jar:lwjgl_util.jar:lwjgl_util_applet.jar:lzma.jar:xray.jar:snakeyaml-1.9.jar -Djava.library.path=. com.apocalyptech.minecraft.xray.XRay 2>&1 | tee -a ${LOGFILE}
+java -Xms256m -Xmx1024m -Djava.library.path=lib/native -jar xray.jar 2>&1 | tee -a ${LOGFILE}
 
 echo
 echo "X-Ray log saved to ${LOGFILE}"
