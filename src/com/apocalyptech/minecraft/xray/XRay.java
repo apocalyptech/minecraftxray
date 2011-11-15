adb4f81d8ea99fd3ae08b51b561645bd4eb78dbe
CJ Kucera <pez@apocalyptech.com>
2011-11-15 00:03:52 -0600

    Still a ways off, but this will become 3.5.0

--- src/com/apocalyptech/minecraft/xray/XRay.java
+++ src/com/apocalyptech/minecraft/xray/XRay.java
@@ -110,7 +110,7 @@ public class XRay
 	// are we inverting the mouse
 	private boolean invertMouse = false;
 	// window title
-	public static String app_version = "3.4.3";
+	public static String app_version = "3.5.0";
 	public static String app_name    = "Minecraft X-Ray";
 	public static String windowTitle = app_name + " " + app_version;
 
